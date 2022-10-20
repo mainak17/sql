540e12376a8b8dfc7d21af4e29afbc87ea9eafcb
Mainak Das <mainak97@outlook.com>
2022-10-20 22:03:29 +0530

    added solutions

--- Hackerrank/solution-sql-basics.sql
+++ Hackerrank/solution-sql-basics.sql
@@ -5,14 +5,28 @@ select id,name,countrycode,district,population from city where countrycode = 'US
 select name from city where countrycode = 'usa' and population > 120000;
 
 -- Solution 3
+select * from city;
 
 -- Solution 4
+select * from city where id = 1661; 
+
 -- Solution 5
+select * from city where countrycode = 'JPN';      
+
 -- Solution 6
+select name from city where countrycode = 'JPN';   
+
 -- Solution 7
+select city,state from station;     
+
 -- Solution 8
+select distinct city from station where mod(id,2)=0 order by city;
+
 -- Solution 9
+select count(city) - count(distinct city) from station; 
+
 -- Solution 10
+
 -- Solution 11
 -- Solution 12
 -- Solution 13
