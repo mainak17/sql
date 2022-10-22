7e87473cce00c135b9a2668131ab835ec585e044
Mainak Das <mainak97@outlook.com>
2022-10-22 23:33:10 +0530

    Add files via upload

--- Datalemur/solutions-easy.sql
+++ Datalemur/solutions-easy.sql
@@ -4,6 +4,20 @@ FROM candidates where skill in('Python','Tableau','PostgreSQL')
 group by candidate_id
 having count(candidate_id)=3;
 
+--Page With No Likes
+select distinct p.page_id from pages p 
+LEFT JOIN 
+page_likes pl 
+on p.page_id=pl.page_id where liked_date is null
+order by page_id;
+
+--Unfinished Parts
+SELECT distinct part FROM parts_assembly where finish_date is null;
+
+-- Laptop vs. Mobile Viewership
+select distinct (SELECT COUNT(1) FROM viewership where device_type = 'laptop') as laptop_views,
+(SELECT COUNT(1) FROM viewership where device_type != 'laptop') as mobile_views from viewership;
+
 -- Cities With Completed Trades
 SELECT u.city,count(*) as total_orders FROM trades t
 LEFT JOIN
@@ -13,3 +27,4 @@ where t.status = 'Completed'
 GROUP BY u.city
 order by count(*) desc
 limit 3;
+
