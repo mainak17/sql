eda5b5b35998f0529329fb34b22a6d0747557df9
Mainak Das <mainak97@outlook.com>
2022-10-23 08:24:53 +0530

    Add files via upload

--- Datalemur/solutions-medium.sql
+++ Datalemur/solutions-medium.sql
@@ -10,3 +10,17 @@ from
   from transactions
 ) tbl
 where rnk = 3;
+
+--Compensation Outliers
+select * from (
+select tbl.employee_id,tbl.salary,
+CASE
+    WHEN salary > 2*avg THEN 'Overpaid'
+    WHEN salary < avg/2 THEN 'Underpaid'
+END AS status
+from 
+(SELECT * FROM employee_pay ep
+LEFT JOIN
+(SELECT title, avg(salary) from employee_pay GROUP BY title) sal
+on ep.title = sal.title) tbl ) tbl1
+where status is not null;
